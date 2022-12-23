854ca7f8619c754806b600ec46e11ce25bcec653
headshigh <93933601+headshigh@users.noreply.github.com>
2022-12-23 07:15:11 +0545

    akjf

--- controllers/transactioncontroller.js
+++ controllers/transactioncontroller.js
@@ -11,13 +11,82 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 const Transaction = require("../models/Transaction");
-const getalltransaction = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const page = Number(req.query.page) || 1;
-    const skip = (page - 1) * 20;
+const moment = require("moment-timezone");
+const getbalance = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const result = Transaction.find({});
-        const result1 = yield result.sort("-createdAt").skip(skip);
-        res.status(200).json({ msg: result1 });
+        var x = 0;
+        const forbalance = yield Transaction.aggregate([
+            {
+                $group: {
+                    _id: "$transactiontype",
+                    total: {
+                        $sum: "$totalprice",
+                    },
+                },
+            },
+        ]);
+        function findbal() {
+            forbalance.forEach((element) => {
+                if (element._id == "deposit") {
+                    x = x + element.total;
+                }
+                else {
+                    x = x - element.total;
+                }
+                return x;
+            });
+        }
+        findbal();
+        res.status(200).json({ balance: x });
+    }
+    catch (err) {
+        res.status(500).json({ msg: err });
+        console.log(err);
+    }
+});
+const getalltransaction = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const limit = 20;
+    const page = Number(req.query.page) || 1;
+    const skip = (page - 1) * limit;
+    console.log("query", req.query);
+    try {
+        var result;
+        if (req.query.transactionby) {
+            result = Transaction.find({
+                transactionby: req.query.transactionby,
+            });
+        }
+        else {
+            result = Transaction.find({});
+        }
+        const result1 = yield result.sort("-createdAt").skip(skip).limit(limit);
+        var x = 0;
+        const forbalance = yield Transaction.aggregate([
+            {
+                $group: {
+                    _id: "$transactiontype",
+                    total: {
+                        $sum: "$totalprice",
+                    },
+                },
+            },
+        ]);
+        function findbal() {
+            forbalance.forEach((element) => {
+                if (element._id == "deposit") {
+                    x = x + element.total;
+                }
+                else {
+                    x = x - element.total;
+                }
+                return x;
+            });
+        }
+        findbal();
+        res.status(200).json({
+            msg: result1,
+            balance: x,
+        });
     }
     catch (err) {
         console.log(err);
@@ -29,12 +98,45 @@ const createtransaction = (req, res) => __awaiter(void 0, void 0, void 0, functi
     console.log(req.body);
     try {
         var date = new Date();
+        var x = 0;
+        const forbalance = yield Transaction.aggregate([
+            {
+                $group: {
+                    _id: "$transactiontype",
+                    total: {
+                        $sum: "$totalprice",
+                    },
+                },
+            },
+        ]);
+        function findbal() {
+            forbalance.forEach((element) => {
+                if (element._id == "deposit") {
+                    x = x + element.total;
+                }
+                else {
+                    x = x - element.total;
+                }
+                return x;
+            });
+        }
+        findbal();
+        var balanceaftertrans;
+        if (transactiontype == "deposit") {
+            balanceaftertrans = x + totalprice;
+        }
+        else {
+            balanceaftertrans = x - totalprice;
+        }
+        const datenepal = moment.tz(Date.now(), "Asia/Kathmandu");
         const result = yield Transaction.create({
             nameofitem: nameofitem,
             totalprice: totalprice,
             remarks: remarks,
             transactionby: transactionby,
             transactiontype: transactiontype,
+            balanceafter: balanceaftertrans,
+            transactiontime: datenepal,
         });
         if (result) {
             return res.status(200).json({ result });
@@ -50,4 +152,4 @@ const createtransaction = (req, res) => __awaiter(void 0, void 0, void 0, functi
         res.status(500).json({ err: err });
     }
 });
-module.exports = { createtransaction, getalltransaction };
+module.exports = { createtransaction, getalltransaction, getbalance };
